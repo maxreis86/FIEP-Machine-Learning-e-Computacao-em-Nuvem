4e63f745c9db3d2fbaef59b65049e2993469433d
maxreis86 <maxreis86@gmail.com>
2022-12-08 19:08:35 -0300

    postgres connection

---
+++ aula_02_titanic_kmeans/postgres_connection.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+import pandas as pd
+import psycopg2
+
+df = pd.read_csv('https://raw.githubusercontent.com/maxreis86/FIEP-Machine-Learning-e-Computacao-em-Nuvem/main/aula_01_titanic_h2o_automl/titanic/train.csv')
+
+engine = create_engine('postgresql://user:password@fiep-postgres.postgres.database.azure.com:5432/postgres')
+df.to_sql('titanic', engine, if_exists='replace')
+
+print(pd.read_sql_query('select * from titanic', engine))
